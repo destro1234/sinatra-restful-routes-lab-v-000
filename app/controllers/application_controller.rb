00ed2193fe05661934aae8d30fa99767ab3bec0d
Elias Ventura <eliasventura23@gmail.com>
2019-03-18 02:20:00 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,51 +1,49 @@
-require './config/environment'
-
 class ApplicationController < Sinatra::Base
-  # register Sinatra::ActiveRecordExtension
-  set :views, Proc.new { File.join(root, "../views/") }
-
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
   end
 
-  get '/recipes/new' do #loads new form
-    erb :new
-  end
-
-  get '/recipes' do #loads index page
+  get '/recipes' do
     @recipes = Recipe.all
     erb :index
   end
 
-  get '/recipes/:id' do  #loads show page
-    @recipe = Recipe.find_by_id(params[:id])
+  get '/recipes/new' do
+    erb :new
+  end
+
+
+
+  # code actions here!
+  get '/recipes/:id' do
+    @recipe = Recipe.find(params[:id])
     erb :show
   end
 
-  get '/recipes/:id/edit' do #loads edit form
-    @recipe = Recipe.find_by_id(params[:id])
+  get '/recipes/:id/edit' do
+    @recipe = Recipe.find(params[:id])
     erb :edit
   end
 
-  patch '/recipes/:id' do  #updates a recipe
-    @recipe = Recipe.find_by_id(params[:id])
+  patch '/recipes/:id/' do
+    @recipe = Recipe.find(params[:id])
     @recipe.name = params[:name]
     @recipe.ingredients = params[:ingredients]
     @recipe.cook_time = params[:cook_time]
     @recipe.save
-    redirect to "/recipes/#{@recipe.id}"
+    redirect "recipes/#{@recipe.id}"
+
   end
 
-  post '/recipes' do  #creates a recipe
-    @recipe = Recipe.create(params)
-    redirect to "/recipes/#{@recipe.id}"
+  post '/recipes' do
+    @recipe = Recipe.create(:name=> params[:name], :ingredients=> params[:ingredients], :cook_time=> params[:cook_time])
+    redirect "/recipes/#{@recipe.id}"
   end
 
-  delete '/recipes/:id' do #destroy action
-    @recipe = Recipe.find_by_id(params[:id])
+  delete '/recipes/:id' do
+    @recipe = Recipe.find(params[:id])
     @recipe.delete
-    redirect to '/recipes'
+    redirect '/recipes'
   end
-
 end
